39d9bbe0a3caeee4b9d0f814045fc919208055e0
Sliman-baghouri <slimanbusiness@gmail.com>
2024-12-09 11:32:46 +0100

    x

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,23 @@ module.exports = {
         /* also pick nested css from theme */
         "../../assets/css/*.css",
     ],
-    // ...
+    theme: {
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'], // Default sans font
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#05747A', // Default shade
+          light: '#E6F5F6',  // Lighter shade
+          dark: '#DADADA'
+        },
+        accent: {
+          DEFAULT: '#E6F5F6',
+        },
+      },
+    },
+  },
 }
+
+
