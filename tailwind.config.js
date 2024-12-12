4e9d12bd6cd53776b603bfeb018ec7ac171bae76
Sliman-baghouri <slimanbusiness@gmail.com>
2024-12-12 15:03:04 +0100

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,30 @@
 module.exports = {
-    content: [
+  content: [
+    /* relevant files from the blog + theme */
+    "../../content/**/*.{html,md}",
+    "../../layouts/**/*.html",
 
-		/* relevant files from the blog + theme */
-        "../../content/**/*.{html,md}",
-        "../../layouts/**/*.html",
+    /* relevant files from the theme */
+    "./layouts/**/*.html",
 
-		/* relevant files from the theme */
-        "./layouts/**/*.html",
-
-        /* also pick nested css from theme */
-        "../../assets/css/*.css",
-    ],
-    theme: {
+    /* also pick nested css from theme */
+    "../../assets/css/*.css",
+  ],
+  theme: {
     extend: {
       fontFamily: {
-        sans: ['DM Sans', 'sans-serif'], // Default sans font
+        sans: ["Inter", "sans-serif"], // Default sans font
       },
       colors: {
         primary: {
-          DEFAULT: '#05747A', // Default shade
-          light: '#E6F5F6',  // Lighter shade
-          dark: '#DADADA'
+          DEFAULT: "#05747A", // Default shade
+          light: "#E6F5F6", // Lighter shade
+          dark: "#DADADA",
         },
         accent: {
-          DEFAULT: '#E6F5F6',
+          DEFAULT: "#E6F5F6",
         },
       },
     },
   },
-}
-
-
+};
